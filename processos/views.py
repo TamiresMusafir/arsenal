52370932b1d3602bc42cefc33e18d9739d7184e3
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-21 13:55:59 -0300

    Update views.py

--- processos/views.py
+++ processos/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def processos(request):
     return render(request, "processos.html")
+
+def novo_processo(request):
+    return render(request, "novoprocesso.html")
+
+def documentos(request):
+    return render(request, "documentos.html")
+
+def mapas_gerados(request):
+    return render(request, "mapasgerados.html")
