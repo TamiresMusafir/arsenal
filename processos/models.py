76ee2fe329cf458b2d1dc85a976d3b8c87222778
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-21 15:01:31 -0300

    Update models.py

--- processos/models.py
+++ processos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Processo(models.Model):
+    numero = models.CharField(max_length=20)
+    descricao = models.CharField(max_length=255)
+    data_abertura = models.DateField()
+    valor_estimado = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return self.numero
