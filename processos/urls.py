ca37d6db0b957bb9acc4418d49961dabd6e42e40
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-21 13:54:45 -0300

    Update urls.py

--- processos/urls.py
+++ processos/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.processos, name="processos"),
+    path("/novo", views.novo_processo, name="novo_processo"),
+    path("/documentos", views.documentos, name="documentos"),
+    path("/mapasgerados", views.documentos, name="mapas_gerados"),
 ]
