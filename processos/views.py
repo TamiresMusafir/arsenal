a328c791391e19cd46975d50173cdd2fd5554dc1
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-22 11:03:19 -0300

    Update views.py

--- processos/views.py
+++ processos/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Processo
 
 # Create your views here.
 def processos(request):
-    return render(request, "processos.html")
+    processos = Processo.objects.all()
+    return render(request, "processos.html", {"processos": processos})
 
 def novo_processo(request):
+    print("ENTROU NA VIEW")
+    print(request.method)
+
+    if request.method == 'POST':
+        print("ENTROU NO POST")
+        print(request.POST)
+
+        Processo.objects.create(numero = request.POST["numero"],
+                                descricao = request.POST["descricao"],
+                                valor_estimado = request.POST["valor_estimado"])
+                                
+        return redirect("processos")
+    
     return render(request, "novoprocesso.html")
 
 def documentos(request):
