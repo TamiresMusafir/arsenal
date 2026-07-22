d65ff9701c72eeb63264b1f338edea69125c2de1
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-22 11:07:29 -0300

    Update views.py

--- processos/views.py
+++ processos/views.py
@@ -3,21 +3,15 @@ from .models import Processo
 
 # Create your views here.
 def processos(request):
-    processos = Processo.objects.all()
+    processos = Processo.objects.all().order_by("-id")
     return render(request, "processos.html", {"processos": processos})
 
 def novo_processo(request):
-    print("ENTROU NA VIEW")
-    print(request.method)
-
     if request.method == 'POST':
-        print("ENTROU NO POST")
-        print(request.POST)
-
         Processo.objects.create(numero = request.POST["numero"],
                                 descricao = request.POST["descricao"],
                                 valor_estimado = request.POST["valor_estimado"])
-                                
+
         return redirect("processos")
     
     return render(request, "novoprocesso.html")
