51cedcc005c14640f4ac4d34bffc887d38ab8c52
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-22 11:03:31 -0300

    Update models.py

--- processos/models.py
+++ processos/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Processo(models.Model):
     numero = models.CharField(max_length=20)
     descricao = models.CharField(max_length=255)
-    data_abertura = models.DateField()
-    valor_estimado = models.DecimalField(max_digits=15, decimal_places=2)
+    data_abertura = models.DateField(default=timezone.now)
+    valor_estimado = models.DecimalField(max_digits=15, decimal_places=2, null=True,
+    blank=True)
 
     def __str__(self):
         return self.numero
+
