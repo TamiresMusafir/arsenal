ef68e475a9322dfc9093c124640e1ece1c9f6d2f
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-23 14:06:44 -0300

    Update models.py

--- processos/models.py
+++ processos/models.py
@@ -7,9 +7,7 @@ class Processo(models.Model):
     numero = models.CharField(max_length=20)
     descricao = models.CharField(max_length=255)
     data_abertura = models.DateField(default=timezone.now)
-    valor_estimado = models.DecimalField(max_digits=15, decimal_places=2, null=True,
-    blank=True)
+    valor_estimado = models.DecimalField(max_digits=15, decimal_places=2)
 
     def __str__(self):
         return self.numero
-
