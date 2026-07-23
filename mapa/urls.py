7dd1ee1d6136e4832205e9205bf016822f702b53
Victor Cezar <115908435+VictorCezar212@users.noreply.github.com>
2026-07-23 10:29:33 -0300

    Add new URL paths for upload and download views

--- mapa/urls.py
+++ mapa/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.mapa, name="mapa"),
-]
+    path("processar-upload/", views.processar_upload, name="processar_upload"),
+    path("baixar-modelo-base/", views.baixar_modelo_base, name="baixar_modelo_base"),
+]   
