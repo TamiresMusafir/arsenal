f3049b3376aaa45c7232b9ec507f523a2bddd9c5
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-23 14:08:12 -0300

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from processos.models import Processo
 # Create your views here.
 
 def dashboard(request):
-    return render(request, "dashboard.html")
+    processos = Processo.objects.all().order_by("-id")[:5]
+    total_processos = Processo.objects.count()
+
+    return render(request, "dashboard.html", {"processos": processos, "total_processos": total_processos})
