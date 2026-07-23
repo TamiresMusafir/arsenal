89198095f3620c1bafa1e5661931c8d40d5a288f
Victor Cezar <115908435+VictorCezar212@users.noreply.github.com>
2026-07-23 10:28:03 -0300

    Update static and media file settings in Django
    Updated STATIC_URL to include leading slash and added MEDIA_URL and MEDIA_ROOT settings for media file handling. Created MEDIA_TEMP_DIR for temporary uploads.

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,8 +121,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static-assets',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files (Uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Criar pastas necessárias
+MEDIA_TEMP_DIR = os.path.join(MEDIA_ROOT, 'temp')
+os.makedirs(MEDIA_TEMP_DIR, exist_ok=True)
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
