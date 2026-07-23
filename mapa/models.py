1d89d25f6390d04260c0cf56677e694bf00990d2
Victor Cezar <115908435+VictorCezar212@users.noreply.github.com>
2026-07-23 10:29:10 -0300

    Add Mapa and DadosMapa models with fields

--- mapa/models.py
+++ mapa/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Mapa(models.Model):
+    titulo = models.CharField(max_length=100)
+    arquivo = models.FileField(upload_to='arquivos/', null=True, blank=True)
+    data_upload = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+class DadosMapa(models.Model):
+    mapa = models.ForeignKey(Mapa, on_delete=models.CASCADE, related_name='dados')
+    linha = models.IntegerField()
+    coluna = models.CharField(max_length=255)
+    valor =  models.TextField()
+
+    def __str__(self):
+        return f"{self.mapa.titulo} - Linha {self.linha} - {self.coluna}"
