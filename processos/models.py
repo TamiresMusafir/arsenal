fa0d2ca70949b7e9f45feee5b62b29516af5739a
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-27 13:18:26 -0300

    Update models.py

--- processos/models.py
+++ processos/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 class Processo(models.Model):
     numero = models.CharField(max_length=20, unique=True)
     descricao = models.CharField(max_length=255)
-    data_abertura = models.DateField(default=timezone.now)
+    data_abertura = models.DateField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
     valor_estimado = models.DecimalField(max_digits=15, decimal_places=2)
     arquivo_processo = models.FileField(upload_to='processos/', null=True, blank=True)
-    data_criacao = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, default='pendente')
     arquivo_gerado_xlsx = models.FileField(upload_to='processos/gerados/', null=True, blank=True)
     arquivo_gerado_odt = models.FileField(upload_to='processos/gerados/', null=True, blank=True)
