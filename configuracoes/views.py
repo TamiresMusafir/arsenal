eb463d807abfd6b12254a67c9748899e3fd46cdd
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-30 10:50:58 -0300

    Update views.py

--- configuracoes/views.py
+++ configuracoes/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
+@login_required
 def configuracoes(request):
     return render(request, "configuracoes.html")
 
+@login_required
 def conta(request):
-    return render(request, "conta.html")
+    usuario = request.user
 
+    if request.method == "POST":
+        usuario.first_name = request.POST["nome"]
+        usuario.last_name = request.POST["sobrenome"]
+        usuario.email = request.POST["email"]
+
+        usuario.save()
+
+    return render(request, "conta.html", {"usuario": usuario})
+
+@login_required
 def preferencias(request):
     return render(request, "preferencias.html")
