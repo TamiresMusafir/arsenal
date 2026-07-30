7f562fd85a11340bb773eea553e0f9748f83b45d
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-30 10:51:19 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Contexto global do perfil do usuário
+                'usuarios.context_processors.perfil_usuario',
             ],
         },
     },
