720814cdfab7bb5e36b72835c57895fceb1d6893
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-30 10:50:23 -0300

    Update urls.py

--- configuracoes/urls.py
+++ configuracoes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.configuracoes, name="configuracoes"),
     path("conta/", views.conta, name="conta"),
-    path("preferencias/", views.preferencias, name="preferencias")
+    path("preferencias/", views.preferencias, name="preferencias"),
+    path("alterar-senha/", auth_views.PasswordChangeView.as_view(template_name="alterarsenha.html", success_url="/configuracoes/conta/"), name="alterar_senha")
 ]
