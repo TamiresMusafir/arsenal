35cb210becf18e28842f9e87491a01f176e47e2f
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-31 10:28:20 -0300

    Update theme.js

--- static-assets/js/theme.js
+++ static-assets/js/theme.js
@@ -1,23 +1,13 @@
 const switchTheme = document.getElementById("darkModeSwitch");
 
-// Carrega a preferência salva
-if (localStorage.getItem("tema") === "dark") {
+if (switchTheme.checked) {
     document.body.classList.add("dark-theme");
-
-    if (switchTheme) {
-        switchTheme.checked = true;
-    }
 }
 
-// Quando clicar no switch
 switchTheme?.addEventListener("change", () => {
-
-  if (switchTheme.checked) {
-      document.body.classList.add("dark-theme");
-      localStorage.setItem("tema", "dark");
-  } else {
-      document.body.classList.remove("dark-theme");
-      localStorage.setItem("tema", "light");
-  }
-
+    if (switchTheme.checked) {
+        document.body.classList.add("dark-theme");
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
 });
