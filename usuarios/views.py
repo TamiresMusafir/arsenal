c8f65c057da82be36c02448a32f960ff8f6d35b5
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-31 09:58:14 -0300

    Update views.py

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
-# Create your views here.
 
 def login_view(request):
-  if request.method == "POST":
-    username = request.POST["username"]
-    password = request.POST["password"]
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
 
-    usuario = authenticate(request, username=username, password=password)
+        usuario = authenticate(request, username=username, password=password)
 
-    if usuario is not None:
-      login(request, usuario)
-      return redirect("home")
+        if usuario is not None:
+            login(request, usuario)
+            return redirect("home")
 
-  return render(request, "login.html")
+    return render(request, "login.html")
+
+
+class AlterarSenha(auth_views.PasswordChangeView):
+    template_name = "alterarsenha.html"
+    success_url = reverse_lazy("conta")
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+
+        for field in form.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+        return form
