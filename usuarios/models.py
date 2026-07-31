1b5a87ff54fae22542118a26058da268bd5b2862
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-31 10:30:41 -0300

    Update models.py

--- usuarios/models.py
+++ usuarios/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     foto = models.ImageField(upload_to="perfil/", null=True, blank=True)
+    tema_escuro = models.BooleanField(default=False)
 
     def __str__(self):
         return self.usuario.username
