6212ae46ae98c69594531358c7c0e9867969b741
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-07-31 10:55:45 -0300

    Update urls.py

--- configuracoes/urls.py
+++ configuracoes/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.configuracoes, name="configuracoes"),
     path("conta/", views.conta, name="conta"),
     path("preferencias/", views.preferencias, name="preferencias"),
+    path("alterar-tema/", views.alterar_tema, name="alterar_tema"),
     path("alterar-senha/", AlterarSenha.as_view(), name="alterar_senha"),
     path("editar-conta/", views.editar_conta, name="editar_conta")
 ]
