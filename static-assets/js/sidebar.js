1de296474b575f57d3bf6a258a53ca9d33f4b9f7
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-08-04 10:28:42 -0300

    Update sidebar.js

--- static-assets/js/sidebar.js
+++ static-assets/js/sidebar.js
@@ -1,24 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const btn = document.getElementById("mobileMenuBtn");
-    const menu = document.querySelector(".sidebar");
+    const sidebar = document.querySelector(".sidebar");
     const close = document.querySelector(".close-menu");
     const backdrop = document.querySelector(".sidebar-backdrop");
 
     function abrirMenu() {
-        menu.classList.add("open");
+        sidebar.classList.add("open");
         backdrop.classList.add("show");
         document.body.classList.add("menu-open");
     }
 
     function fecharMenu() {
-        menu.classList.remove("open");
+        sidebar.classList.remove("open");
         backdrop.classList.remove("show");
         document.body.classList.remove("menu-open");
     }
 
-    btn.addEventListener("click", abrirMenu);
-    close.addEventListener("click", fecharMenu);
-    backdrop.addEventListener("click", fecharMenu);
+    btn?.addEventListener("click", abrirMenu);
+    close?.addEventListener("click", fecharMenu);
+    backdrop?.addEventListener("click", fecharMenu);
+
+    const toggle = document.getElementById("perfilToggle");
+    const menuPerfil = document.getElementById("menuPerfil");
+
+
+    console.log(toggle);
+    console.log(menuPerfil);
+
+    if (toggle && menuPerfil) {
+
+        toggle.addEventListener("click", (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            menuPerfil.classList.toggle("show");
+        });
+
+        document.addEventListener("click", (e) => {
+            if (!e.target.closest(".perfil")) {
+                menuPerfil.classList.remove("show");
+            }
+        });
+
+    }
 
 });
