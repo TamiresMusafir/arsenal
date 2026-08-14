199e76e64f0b48f2d60700f9d51bfc1717231424
Victor Cezar <115908435+VictorCezar212@users.noreply.github.com>
2026-08-14 14:08:20 -0300

    Update urls.py

--- processos/urls.py
+++ processos/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.processos, name="processos"),
+    path("novo/", views.novo_processo, name="novo_processo"),
     path("documentos/", views.documentos, name="documentos"),
     path("mapasgerados/", views.mapas_gerados, name="mapas_gerados"),
-    path("novo/", views.novo_processo, name="novo_processo"),
-    path("<str:numero_slug>/", views.visualizar_processo, name="visualizar_processo"),
-    path("<str:numero_slug>/editar/", views.editar_processo, name="editar_processo"),
-    path("download/<str:tipo>/<int:processo_id>/", views.download_arquivo, name="download_arquivo"),
+    path("download/<str:tipo>/<int:processo_id>/",
+         views.download_arquivo, name="download_arquivo"),
 ]
