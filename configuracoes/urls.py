d011526057f3a92399fd5006a87db254b76d8374
Victor Cezar <115908435+VictorCezar212@users.noreply.github.com>
2026-08-18 13:26:43 -0300

    Add backup download and restore paths to URLs

--- configuracoes/urls.py
+++ configuracoes/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("", views.configuracoes, name="configuracoes"),
     path("conta/", views.conta, name="conta"),
     path("preferencias/", views.preferencias, name="preferencias"),
-    path("alterar-tema/", views.alterar_tema, name="alterar_tema"),
+    path("backup/download/", views.backup_download, name="backup_download"),
+    path("backup/restaurar/", views.backup_restore, name="backup_restore"),
     path("alterar-senha/", AlterarSenha.as_view(), name="alterar_senha"),
-    path("editar-conta/", views.editar_conta, name="editar_conta")
+    path("editar-conta/", views.editar_conta, name="editar_conta"),
 ]
