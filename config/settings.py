8f55bb5bed2c5e557bea7eed1efc36820d52428b
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-08-19 16:14:03 +0000

    alteracoes feitas

--- config/settings.py
+++ config/settings.py
@@ -111,9 +111,9 @@ DATABASES = {
         #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',      # >>> ALTERADO
         'NAME': os.environ.get('DB_NAME', 'postgres'),
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
         'PORT': os.environ.get('DB_PORT', '5432'),
         'CONN_MAX_AGE': 0,
         'OPTIONS': {'sslmode': 'require'},
@@ -275,6 +275,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static-assets',
 ]
