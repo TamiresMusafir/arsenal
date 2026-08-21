d09502fbafb0a04298216a8addf46bd99709a984
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-08-21 13:48:05 +0000

    bugs resolvidos

--- static-assets/js/theme.js
+++ static-assets/js/theme.js
@@ -1,11 +1,16 @@
 const switchTheme = document.getElementById("darkModeSwitch");
 
 if (switchTheme) {
+    switchTheme.checked = document.body.classList.contains("dark-theme");
+
     switchTheme.addEventListener("change", () => {
+
         if (switchTheme.checked) {
             document.body.classList.add("dark-theme");
         } else {
             document.body.classList.remove("dark-theme");
         }
+
     });
+
 }
